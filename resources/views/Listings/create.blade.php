f48a9e226e1ab07665da0cdf77ec9b9a825d127d
momoelabdi <momo.elabdi@bluewin.ch>
2022-12-06 21:34:49 +0100

    strore forms

--- resources/views/Listings/create.blade.php
+++ resources/views/Listings/create.blade.php
@@ -2,31 +2,31 @@
 
 @section('content')
     <div class="create-form">
-        <form method="POST" action="/listing" enctype="multipart/form-data">
+        <form method="POST" action="/listings" enctype="multipart/form-data">
             @csrf
             <div class="hoster-form">
                 <label> Hoster </label>
-                <input type="text" name="Hoster" placeholder="California beach">
+                <input type="text" name="hoster" placeholder="California beach" value="{{ old('hoster') }}">
             </div>
             <div class="hoster-form">
                 <label> where you Sleep </label>
-                <input type="text" name="title" placeholder="Jhon Doe familly">
+                <input type="text" name="title" placeholder="Jhon Doe familly" value="{{ old('title') }}">
             </div>
             <div class="hoster-form">
                 <label> Location </label>
-                <input type="text" name="location" placeholder="Clifornia beach 73, steert 32, US">
+                <input type="text" name="location" placeholder="Clifornia beach 73, steert 32, US" value="{{ old('location') }}">
             </div>
             <div class="hoster-form">
                 <label>Meals we offer</label>
-                <input type="text" name="tags" placeholder="Dinner">
+                <input type="text" name="tags" placeholder="Dinner" value="{{ old('tags') }}">
             </div>
             <div class="hoster-form">
                 <label> Picture </label>
-                <input type="file" name="logo" placeholder="Clifornia beach 73, steert 32, US">
+                <input type="file" name="logo" placeholder="Clifornia beach 73, steert 32, US" value="{{ old('logo') }}">
             </div>
             <div class="hoster-form">
                 <label>Description</label>
-                <textarea type="text" name="decription" placeholder="Amazing place to be"></textarea>
+                <textarea type="text" name="decription" placeholder="Amazing place to be" value="{{ old('decription') }}"></textarea>
                 <input type="submit" value="Submit">
             </div>
 
