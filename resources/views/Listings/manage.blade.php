e6d89c7382a91fe38c848571dc3b115bcfb6607d
momoelabdi <momo.elabdi@bluewin.ch>
2022-12-19 15:12:57 +0100

    style posts

--- resources/views/Listings/manage.blade.php
+++ resources/views/Listings/manage.blade.php
@@ -2,27 +2,31 @@
 
 @section('content')
 
-<h1>Hello </h1>
-<table>
-    @unless ($listing->isEmpty())
-        @foreach ($listing as $item)
-            <tbody>
-                <tr>
-                    <td>{{$item->title}}</td>
-                    <td><a href="/listing/{{$item->id}}/edit">Edit</a></td>
-                    <td><form method="POST" action="/listings/{{$item->id}}">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">Delete</button>
-                    </form></td>
-
-                </tr>
-            </tbody>
-        @endforeach
-    @endunless
-</table>
-
-
-
-
+    <h1>Hello </h1>
+    <table class="host-table">
+        @unless($listing->isEmpty())
+        <tr>
+            <th> My Hosting</th>
+            <th> Edit</th>
+            <th>Delete</th>
+        </tr>
+            @foreach ($listing as $item)
+                <tbody>
+                    <tr>
+                        <td>{{ $item->title }}</td>
+                        <td><a class="manage-btn edit" href="/listing/{{ $item->id }}/edit">Edit</a></td>
+                        <td>
+                            <form method="POST" action="/listings/{{ $item->id }}">
+                                @csrf
+                                @method('DELETE')
+                                <button class="manage-btn" type="submit">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                </tbody>
+            @endforeach
+        @else
+            <h3 class="message">No Listings Found</h3>
+        @endunless
+    </table>
 @endsection
