5b40ae8e45caaf412c19f6eee1461eee214d6fb8
momoelabdi <momo.elabdi@bluewin.ch>
2023-01-04 10:52:06 +0100

    upcomming trips

--- resources/views/reservations/manage.blade.php
+++ resources/views/reservations/manage.blade.php
@@ -1,15 +1,19 @@
 @extends('layout')
 @section('content')
     <div class="reservation-card">
+        {{-- @foreach ($listings as $item)
+            <p>{{$item->email}} </p>
+        @endforeach  --}}
+        {{ dd($listing)}}
         @unless($reservations->isEmpty())
             @foreach ($reservations as $reserved)
                 <div class="reserved">
-
+                    <h1>Trips</h1>
+                    <h2>Upcoming reservations</h2>
                     <h2> Dear {{ $reserved->user_name }} your reservation has been submitted</h2>
                     <h2>from {{ Str::limit($reserved->start, 10) }}</h2>
                     <h2>to {{ Str::limit($reserved->end, 10) }}</h2>
-                    <h2>The confirmation has been sent by emmail to the folowing address <address> {{ $reserved->user_email }}
-                        </address>
+                    <h2>The confirmation has been sent by emmail to the folowing address <address> {{ $reserved->user_email }}                       </address>
                     </h2>
                     <p>Enjoy your holiday </p>
                     {{-- <h2>{{ $reserved->listing_id}}</h2> --}}
