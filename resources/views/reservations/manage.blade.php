f4b3c636de59eb58a1a6e9944b48e289f9eab7c8
momoelabdi <momo.elabdi@bluewin.ch>
2023-01-05 13:03:43 +0100

    reservations UI

--- resources/views/reservations/manage.blade.php
+++ resources/views/reservations/manage.blade.php
@@ -1,29 +1,27 @@
 @extends('layout')
 @section('content')
-    <div class="reservation-card">
+    <div class="reservation-container">
         @unless($reservations->isEmpty())
-        @foreach ($reservations as $reserved)
-            <div class="reserved">
-                @foreach ($listings as $listing)
-                    @if ($reserved->listing_id == $listing->id)
-                        <h1>{{ $listing->id }}</h1>
-                        <img src="{{ $listing->logo ? asset('storage/' . $listing->logo) : asset('./images/home.jpg') }}"/>
-                        <h1>{{ $listing->location }}</h1>
-                        <h1>{{ $listing->description }}</h1>
-                        {{-- <h1>{{ $listing->logo }}</h1> --}}
-                    @endif
+        <div class="reservation-card">
+                @foreach ($reservations as $reserved)
+                <div class="reserved">
+                    @foreach ($listings as $listing)
+                    <a href="/item/{{ $listing->id }}">
+                            @if ($reserved->listing_id == $listing->id)
+                                <img
+                                    src="{{ $listing->logo ? asset('storage/' . $listing->logo) : asset('./images/home.jpg') }}" />
+                                <h4>{{ $listing->location }}</h4>
+                                <h4>hosted by {{ $listing->hoster }}</h4>
+                            @endif
+                        @endforeach
+                        <h3>from {{ Str::limit($reserved->start, 10) }}</h3>
+                        <h3>to {{ Str::limit($reserved->end, 10) }}</h3>
+                    </a>
+                    </div>
                 @endforeach
-                <h1>{{ $reserved->message }}</h1>
-                <h2> Dear {{ $reserved->user_name }} your reservation has been submitted</h2>
-                <h2>from {{ Str::limit($reserved->start, 10) }}</h2>
-                <h2>to {{ Str::limit($reserved->end, 10) }}</h2>
-                <h2>The confirmation has been sent to email to the folowing address <address>
-                        {{ $reserved->user_email }} </address>
-                </h2>
             </div>
-        @endforeach
-        @else
-            <h3 style="padding-top:20%" class="message">No reservations Found!!</h3>
-        @endunless
-    </div>
+        </div>
+    @else
+        <h3 class="message">No reservations Found!!</h3>
+    @endunless
 @endsection
