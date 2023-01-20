ced261ef190652b313727315054612208aec32e3
momoelabdi <momo.elabdi@bluewin.ch>
2023-01-20 15:42:27 +0100

    ligin forms js issues

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -1,27 +1,23 @@
-
-    
 <div id="login" class="login">
     <form class="modal-content animate" method="POST" action="/users/authenticate">
         <span onclick="hideLogin()" class="close" title="Close Modal">&times;</span>
         @csrf
         <div class="imgcontainer">
-            <img src="./images/user.png" alt="Avatar" class="avatar">
+            {{-- <img src="./images/user.png" alt="Avatar" class="avatar"> --}}
         </div>
         <div class="form-container">
             <label>Your Email</label>
             <input type="text" name="email">
             @error('email')
-            <p id="login-msg" class="message"> {{ $message }} </p>
+                <p id="login-msg" class="message"> {{ $message }} </p>
             @enderror
             <label>Your Password</label>
-            <input type="password" name="password"  autocomplete="on">
+            <input type="password" name="password" autocomplete="on">
             @error('password')
-            <p id="login-msg" class="message"> {{ $message }} </p>
+                <p id="login-msg" class="message"> {{ $message }} </p>
             @enderror
-            <button type="submit">Login</button>
-            {{-- <label>
-                <input type="checkbox" checked="checked" name="remember"> Remember me
-            </label> --}}
+            <button  type="submit">Login</button>
+            <button type="button" onclick="getSignUp()">Sign up</button>
         </div>
         <div class="form-container">
             <button id="login-btn" type="button" onclick="hideLogin()" class="cancelbtn">Cancel</button>
@@ -31,18 +27,24 @@
 
 <script>
     function showLogin() {
-        document.getElementById('login').style.display = 'block'
+        document.getElementById('login').style.display = 'block';
     }
-    
+
     function hideLogin() {
         document.getElementById('login').style.display = 'none'
     }
-
-    if(document.getElementById('login-msg') != null) {  
-        document.getElementById('login').style.display = 'block';
-    }else if (document.getElementById('login-msg') == null) {
-        document.getElementById('login').style.display = 'none';
+    if (document.getElementById('login-msg')!= null) {
+        showLogin();
+        hideSignIn();
     }
-    
-</script>
 
+    function getSignUp() {
+        document.getElementById('login').style.display = 'none';
+        document.getElementById('register').style.display = 'block';
+    }
+
+    
+
+
+   
+</script>
--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -1,62 +1,44 @@
- <div id="register" class="register">
-     <form class="regiter-content animate" method="POST" action="/users">
-         <span onclick="hideSignIn()" class="close" title="Close Modal">&times;</span>
-         @csrf
-         <div class="register-container">
-             <label>Your name</label>
-             <input type="text" name="name" placeholder="Name">
+<div id="register" class="register">
+    <form class="regiter-content animate" method="POST" action="/users">
+        <span onclick="hideSignIn()" class="close" title="Close Modal">&times;</span>
+        @csrf
+        <div class="register-container">
+            <label>Your name</label>
+            <input type="text" name="name" placeholder="Name">
 
-             <label>Your email</label>
-             <input type="email" name="email" placeholder="Email">
-             @error('email')
-                 <p id="register-msg" class="message"> {{ $message }}</p>
-             @enderror
+            <label>Your email</label>
+            <input type="email" name="email" placeholder="Email">
+            @error('email')
+                <p id="register-msg" class="message"> {{ $message }}</p>
+            @enderror
 
-             <label>Your password</label>
-             <input type="password" name="password" placeholder="Password">
-             @error('password')
-                 <p id="register-msg" class="message"> {{ $message }}</p>
-             @enderror
+            <label>Your password</label>
+            <input type="password" name="password" placeholder="Password">
+            @error('password')
+                <p id="register-msg" class="message"> {{ $message }}</p>
+            @enderror
 
-             <label>Confirm password</label>
-             <input type="password" name="password_confirmation" placeholder=" password">
-             @error('password')
-                 <p id="register-msg" class="message"> {{ $message }}</p>
-             @enderror
-             <button type="submit" onclick="registerErr()">Sign in</button>
-             <button type="button" onclick="hideSignIn()" class="cancelbtn">Cancel</button>
-         </div>
-     </form>
- </div>
+            <label>Confirm password</label>
+            <input type="password" name="password_confirmation" placeholder=" password">
+            @error('password')
+                <p id="register-msg" class="message"> {{ $message }}</p>
+            @enderror
+            <button class="signInBtn" type="submit" onclick="getRegister()
+            ">Sign in</button>
+            <button type="button" onclick="hideSignIn()" class="cancelbtn">Cancel</button>
+        </div>
+    </form>
+</div>
 
- <script>
-     function showSignIn() {
-         document.getElementById('register').style.display = 'block';
-     }
-
-     function hideSignIn() {
-         document.getElementById('register').style.display = 'none';
-     }
-
-     if (document.getElementById('register-msg') != null) {
-         document.getElementById('register').style.display = 'block';
-     }
-
-
-
-
-    //  const regs = document.getElementById('register');
-    //  const log = document.getElementById('login');
-    //  const reg = document.getElementById('register-msg');
-    //  const loginMsg = document.getElementById('login-msg');
-    //  const msg = reg.innerHTML;
-    //  const lmsg = loginMsg.innerHTML;
-    //  if (msg != null) {
-    //      log.style.display = "none";
-    //      regs.style.display = "block";
-    //  }
-    //  if (lmsg != null) {
-    //      regs.style.display = "none";
-    //      log.style.display = "block";
-    //  }
- </script>
+<script>
+    function showSignIn() {
+        document.getElementById('register').style.display = 'block';
+    }
+    function hideSignIn() {
+        document.getElementById('register').style.display = 'none';
+    }
+    if (document.getElementById('register-msg') != null) {
+        hideLogin();
+        showSignIn();
+    }
+</script>
