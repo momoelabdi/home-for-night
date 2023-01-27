95ba0027467a1559509b0498dc3b55bab94b0c18
momoelabdi <momo.elabdi@bluewin.ch>
2023-01-27 11:40:05 +0100

    emails fix

--- resources/views/emails/new_reservations.blade.php
+++ resources/views/emails/new_reservations.blade.php
@@ -1,3 +1,9 @@
 
+<h2>You have a new reservation</h2>
 
-You have a new reservation
+
+<p> Your hosting offer on home for night has been reserved.</p>
+<p>Please login and evaluate the hosting request </p>
+
+
+<p>Thank you for using our service</p>
