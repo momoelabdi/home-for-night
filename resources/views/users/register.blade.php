f8516e93e7f110285c83be3c4cfdab875d53918b
momoelabdi <momo.elabdi@bluewin.ch>
2023-01-27 12:31:31 +0100

    footer styl

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -2,6 +2,9 @@
     <form class="regiter-content animate" method="POST" action="/users">
         <span onclick="hideSignUp()" class="close" title="Close Modal">&times;</span>
         @csrf
+        <div class="imgcontainer">
+            <img src="{{ asset('./images/Non-profit logo.png') }}" alt="Avatar" class="main-logo">
+        </div>
         <div class="register-container">
             <label>Your name</label>
             <input type="text" name="name" placeholder="Name" required>
@@ -13,13 +16,13 @@
             @error('email')
                 <p id="register-msg" class="message"> {{ $message }} </p>
             @enderror
-            <label>Your password</label>
+            <label>Your Password</label>
             <input type="password" name="password" placeholder="Password" required>
             @error('password')
                 <p id="register-msg" class="message"> {{ $message }} </p>
             @enderror
-            <label>Confirm password</label>
-            <input type="password" name="password_confirmation" placeholder=" password" required>
+            <label>Confirm Password</label>
+            <input type="password" name="password_confirmation" placeholder=" Confirm Password" required>
             @error('password_confirmation')
                 <p id="register-msg" class="message"> {{ $message }} </p>
             @enderror
