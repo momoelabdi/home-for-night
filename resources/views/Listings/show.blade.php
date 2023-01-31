27aab5ee57f997429e9a0016f9e83d48090914f2
momoelabdi <momo.elabdi@bluewin.ch>
2023-01-31 14:57:40 +0100

    typoo

--- resources/views/Listings/show.blade.php
+++ resources/views/Listings/show.blade.php
@@ -60,32 +60,33 @@
              </div>
          @else
              <div class="create-reservation">
-                 <form method="POST" action="/reservations" enctype="multipart/form-data">
-                     @csrf
-                     <h2>Let's Reserve it Now</h2>
-                     <div class="reservation-form">
-                         <label>Name</label>
-                         <input type="text" placeholder="Full Name">
-                     </div>
-                     <div class="reservation-form">
-                         <label> Email </label>
-                         <input type="email" placeholder="Email" value="">
-                     </div>
-                     <div class="reservation-form">
-                         <label>Start date</label>
-                         <input type="date">
-                     </div>
-                     <div class="reservation-form">
-                         <label>End date</label>
-                         <input type="date">
-                     </div>
-                     <div class="reservation-form">
-                         <label>Message</label>
-                         <textarea type="text" placeholder="Leave a message"></textarea>
-                         <input type="button" onclick="showLogin()" value="Reserve">
-                     </div>
-                 </form>
+                 <div class="spc">
+                     <form method="POST" action="/reservations" enctype="multipart/form-data">
+                         @csrf
+                         <h2>Let's Reserve it Now</h2>
+                         <div class="reservation-form">
+                             <label>Name</label>
+                             <input type="text" placeholder="Full Name">
+                         </div>
+                         <div class="reservation-form">
+                             <label> Email </label>
+                             <input type="email" placeholder="Email" value="">
+                         </div>
+                         <div class="reservation-form">
+                             <label>Start date</label>
+                             <input type="date">
+                         </div>
+                         <div class="reservation-form">
+                             <label>End date</label>
+                             <input type="date">
+                         </div>
+                         <div class="reservation-form">
+                             <label>Message</label>
+                             <textarea type="text" placeholder="Leave a message"></textarea>
+                             <input type="button" onclick="showLogin()" value="Reserve">
+                         </div>
+                     </form>
+                 </div>
              </div>
          @endauth
-     </div>
- @endsection
+     @endsection
